5ce20060c994ea38e6e5fefdbae5c7af583806ec
Alamin-git <alamin2.mas@gmail.com>
2022-03-02 18:42:23 +0600

    single cird showing

--- js/app.js
+++ js/app.js
@@ -42,4 +42,28 @@ const phoneDetiels = phoneId => {
 
 const showPhoneDetiels = phone => {
     console.log(phone);
+    const phoneDetiel = document.getElementById('phone-detiel');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+            <div class="card mb-3" style="max-width: 550px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+    `;
+    phoneDetiel.appendChild(div);
+
+
 }
